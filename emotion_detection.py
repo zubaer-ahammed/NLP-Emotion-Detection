5020d85e84dcb1c6e317924f0d2fae1cc53a8688
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-02-23 02:16:44 -0500

    emotion_detector function and formatted_output

---
+++ emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):    
+    # Define the URL for the emotion detection API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
