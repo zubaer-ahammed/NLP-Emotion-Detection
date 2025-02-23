05aec94b0ba9519d623b373ca89a8b07bc1337f1
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-02-23 03:40:40 -0500

    Static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
@@ -5,6 +10,7 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 @app.route("/sentimentAnalyzer")
 def sent_detector():
+    '''Function to detect the emotion in the given text'''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -18,12 +24,14 @@ def sent_detector():
     return (
         f"For the given statement, the system response is 'anger': {response['anger']}, "
         f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
-        f"and 'sadness': {response['sadness']}. The dominant emotion is <b>{response['dominant_emotion']}</b>."
+        f"and 'sadness': {response['sadness']}. "
+        f"The dominant emotion is <b>{response['dominant_emotion']}</b>."
     )
 
 @app.route("/")
 @app.route("/")
 def render_index_page():
+    '''Render the template to analyze text emotion on the webpage'''
     return render_template('index.html')
 
 if __name__ == "__main__":
